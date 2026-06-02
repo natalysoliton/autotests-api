86532e183cc6d434f1d041a907bbd62a5aa8e9a7
Ermakova Natalia <natalysolitonsf@yandex.com>
2026-06-02 14:42:39 +0300

    9.1 Рефакторинг и подготовка к написанию API автотестов-homework

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 pytest_plugins = (
     "fixtures.users",
     "fixtures.files",
-    "fixtures.courses",  # Добавляем фикстуры для работы с курсами
+    "fixtures.courses",
+    "fixtures.exercises",  # Добавляем фикстуры для работы с заданиями
     "fixtures.authentication"
 )
