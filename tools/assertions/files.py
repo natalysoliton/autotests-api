0290b67613c073e01db112024fcfd0bfaf63e2e6
Ermakova Natalia <natalysolitonsf@yandex.com>
2026-06-03 16:03:53 +0300

    11.1 Настройки автотестов с Pydantic

--- tools/assertions/files.py
+++ tools/assertions/files.py
@@ -1,9 +1,11 @@
+import allure
+
 from clients.errors_schema import ValidationErrorResponseSchema, ValidationErrorSchema, InternalErrorResponseSchema
 from clients.files.files_schema import CreateFileResponseSchema, CreateFileRequestSchema, FileSchema, \
     GetFileResponseSchema
+from config import settings  # Импортируем настройки
 from tools.assertions.base import assert_equal
 from tools.assertions.errors import assert_validation_error_response, assert_internal_error_response
-import allure  # Импортируем allure
 
 @allure.step("Check file")  # Добавили allure шаг
 def assert_file(actual: FileSchema, expected: FileSchema):
@@ -20,7 +22,7 @@ def assert_file(actual: FileSchema, expected: FileSchema):
     assert_equal(actual.directory, expected.directory, "directory")
 
 
-@allure.step("Check create file response")  # Добавили allure шаг
+@allure.step("Check create file response")
 def assert_create_file_response(request: CreateFileRequestSchema, response: CreateFileResponseSchema):
     """
     Проверяет, что ответ на создание файла соответствует запросу.
@@ -29,8 +31,8 @@ def assert_create_file_response(request: CreateFileRequestSchema, response: Crea
     :param response: Ответ API с данными файла.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    # Формируем ожидаемую ссылку на загруженный файл
-    expected_url = f"http://localhost:8000/static/{request.directory}/{request.filename}"
+    # Используем значение хоста из настроек
+    expected_url = f"{settings.http_client.client_url}static/{request.directory}/{request.filename}"
 
     assert_equal(str(response.file.url), expected_url, "url")
     assert_equal(response.file.filename, request.filename, "filename")
